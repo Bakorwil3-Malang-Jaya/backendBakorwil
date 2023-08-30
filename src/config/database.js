0515f34c13657904bd5a22bc3860141ea593d539
dewandra <dewandrarb@gmail.com>
2023-08-30 10:14:11 +0700

    deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-const db = new Sequelize("bkrwl", "root", "", {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql",
 });
